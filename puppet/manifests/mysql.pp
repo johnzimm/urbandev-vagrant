7232cc6ccb07ae8b19a858d9eb03410192ec7de5
J.Zimmerman <john.zimm@gmail.com>
2016-03-17 18:33:45 -0700

    [WIP] Mysql configuration

--- puppet/manifests/mysql.pp
+++ puppet/manifests/mysql.pp
@@ -3,9 +3,23 @@
 #
 class { '::mysql::server': }
 
-mysql::db { 'urbandev':
-  user     => 'urbandev',
-  password => 'vagrant',
+mysql::db { 'urbandev_dev':
+  user     => 'urbandev_dev',
+  password => 'vagrant_dev',
+  host     => 'localhost',
+  grant    => ['ALL'],
+}
+
+mysql::db { 'urbandev_stg':
+  user     => 'urbandev_stg',
+  password => 'vagrant_stg',
+  host     => 'localhost',
+  grant    => ['ALL'],
+}
+
+mysql::db { 'urbandev_prd':
+  user     => 'urbandev_prd',
+  password => 'vagrant_prd',
   host     => 'localhost',
   grant    => ['ALL'],
 }
